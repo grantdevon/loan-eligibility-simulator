ba01d0e78e6171613d8f1a0b3edd7b543973f5bb
grantdevon <grantsolomons62@gmail.com>
2025-09-23 20:48:26 +0200

    Add initial test setup
    - Created App.test.tsx to test the rendering of the App component.
    - Added setupTests.ts for configuring testing-library with jest-dom.
    - Introduced tsconfig.test.json for TypeScript configuration specific to tests.

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '\\.(css|less|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
+    '\\.(gif|ttf|eot|svg)$': '<rootDir>/__mocks__/fileMock.js',
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  testMatch: ['<rootDir>/src/**/*.test.{ts,tsx}'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: 'tsconfig.test.json',
+      useESM: true
+    }]
+  },
+  extensionsToTreatAsEsm: ['.ts', '.tsx']
+};
